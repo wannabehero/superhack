4992123c81692a3c57ac5fbf6abfaf2abf39d54e
Alexander Balaban <balaban.alexander+github@gmail.com>
2023-08-05 15:16:31 +0100

    schema defines

--- packages/libs/src/eas.ts
+++ packages/libs/src/eas.ts
@@ -4,37 +4,34 @@ import { ethers } from 'ethers';
 
 export const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
 
+const key = "haha"
+const provider = ethers.providers.getDefaultProvider(
+    "sepolia"
+);
+const wallet = new ethers.Wallet(key, provider);
+
 class EASClient {
     private eas: EAS
 
     constructor() {
         const eas = new EAS(EASContractAddress);
-        const provider = ethers.providers.getDefaultProvider(
-            "sepolia"
-        );
         // @ts-expect-error
         eas.connect(provider);
         this.eas = eas;
     }
 
-    async createTempateAttestation(signer: SignerOrProvider): Promise<string> {
+    async createTempateAttestation(signer: SignerOrProvider, params: SchemaItem[]): Promise<string> {
         return await this.createAttestation(
             signer, 
-            [
-                { name: "eventId", value: 1, type: "uint256" },
-                { name: "voteIndex", value: 1, type: "uint8" },
-            ],
+            params,
             templateSchema
         );
     }
 
-    async createTempateStatsAttestation(signer: SignerOrProvider): Promise<string> {
-        return await this.createAttestation(
+    async createTempateStatsAttestation(signer: ethers.Wallet, params: SchemaItem[]): Promise<string> {
+        return await this.createOffchainAttestation(
             signer, 
-            [
-                { name: "eventId", value: 1, type: "uint256" },
-                { name: "voteIndex", value: 1, type: "uint8" },
-            ],
+            params,
             templateStatsSchema,
         );
     }
@@ -53,13 +50,34 @@ class EASClient {
         const tx = await this.eas.attest({
             schema: schema.uid,
             data: {
-                recipient: "0xFD50b031E778fAb33DfD2Fc3Ca66a1EeF0652165",
+                recipient: '0xe98bA1B3801d105Ee7C8611E34D9048985b2EFA1',
                 expirationTime: BigInt(0),
-                revocable: true,
+                revocable: false,
                 data: encodedData,
             },
         });
         return await tx.wait();
+    }
+
+    private async createOffchainAttestation(signer: ethers.Wallet, params: SchemaItem[], schema: SchemaDefinition): Promise<string> {
+        const offchain = await this.eas.getOffchain();
+        const encodedData = schema.encodeData(params);
+        const offchainAttestation = await offchain.signOffchainAttestation(
+            {
+                recipient: '0xe98bA1B3801d105Ee7C8611E34D9048985b2EFA1',
+                expirationTime: BigInt(0),
+                time: BigInt(Date.now() / 1000),
+                revocable: true,
+                version: 1,
+                nonce: BigInt(0),
+                schema: schema.uid,
+                refUID: '0x0000000000000000000000000000000000000000000000000000000000000000',
+                data: encodedData,
+            }, 
+            // @ts-expect-error
+            signer,
+        );
+        return offchainAttestation.uid;
     }
 }
 
@@ -74,10 +92,15 @@ class SchemaDefinition extends SchemaEncoder{
 
 // TODO: update with actual data
 const templateSchema = new SchemaDefinition(
-    "0xb16fa048b0d597f5a821747eba64efa4762ee5143e9a80600d0005386edfc995",
-    "uint256 eventId, uint8 voteIndex",
+    "0xe3e791f637267181ffdb97384cac54376f479ae373fe3e709e8bc04e2f67b58a",
+    "bytes[] blob, uint8 version",
 );
 const templateStatsSchema = new SchemaDefinition(
-    "0xb16fa048b0d597f5a821747eba64efa4762ee5143e9a80600d0005386edfc995",
-    "uint256 eventId, uint8 voteIndex",
+    "0xe3e791f637267181ffdb97384cac54376f479ae373fe3e709e8bc04e2f67b58a",
+    "bytes[] blob, uint8 version",
 );
+
+client.createTempateStatsAttestation(wallet as any, [
+    { name: "blob", value: ["0x12"], type: "bytes[]" },
+    { name: "version", value: 13, type: "uint8" },
+])
